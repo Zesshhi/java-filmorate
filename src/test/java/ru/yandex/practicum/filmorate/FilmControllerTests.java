4141bc43cbd3039a083fb23490604db7d384211f
Kirill <96735975+Zesshhi@users.noreply.github.com>
2025-06-08 23:28:29 +0300

    controllers-films-users Создание апи фильмов и пользователей

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
@@ -26,12 +26,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class FilmControllerTests {
 
     String filmJsonCreate = """
-              {
-               "name": "Aboba Film",
-               "description": "Aboba description",
-               "releaseDate": "2000-01-01",
-               "duration": 100
-              }""";
+            {"name": "Aboba Film",
+             "description": "Aboba description",
+             "releaseDate": "2000-01-01",
+             "duration": 100
+            }""";
     @Autowired
     private FilmController filmController;
     @Autowired
